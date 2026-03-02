f77743043a82de83552f374e9950b24da04bbb27
Lzuluaga07 <betiko391@gmail.com>
2026-03-02 14:43:40 -0500

    se termino de procesar y publicar la version final del proyecto el enlace que te envie ya esta actualizado y puedes ver la app funcionando en vivo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'proyecto-react',
   plugins: [react()],
+  base: '/Proyecto-react/', 
 })
