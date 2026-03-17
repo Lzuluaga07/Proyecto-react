a54c3bfbe3dbfe628889cf3ab134d36850357d89
Lzuluaga07 <betiko391@gmail.com>
2026-03-17 09:21:23 -0500

    Instalación y configuración de Bootstrap en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { HashRouter } from 'react-router-dom' // <-- PASO 1: IMPORTAR
 import './index.css'
 import App from './App.jsx'
